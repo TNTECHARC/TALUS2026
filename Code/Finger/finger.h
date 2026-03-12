275bb4ab005945dcb1599bb55f026262bdcd9f1f
Grace Brown <weevilwilson19@gmail.com>
2026-03-12 16:24:54 -0500

    Update finger.h

--- Code/Finger/finger.h
+++ Code/Finger/finger.h
@@ -1,17 +1,3 @@
-#ifndef FINGER_H
-
-#define FINGER_H
-
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
-
-// Create the PCA9685 object (controls the servo driver board)
-extern Adafruit_PWMServoDriver pwm;
-
-// Pulse length limits for HIGH TORQUE MG966R SERVOS
-#define SERVOMIN 75   // pulse for 0Â° 
-#define SERVOMAX 500   // pulse for 180Â°
-
 // Channels where each servo is plugged in on the PCA9685
 extern int servoChannels[];
 /*Finger SETUP for channels:
@@ -23,7 +9,7 @@ extern int servoChannels[];
   WRIST   : 5
 */
 
-// Converts an angle (0â180Â°) into a pulse length the driver understands
+// Converts an angle (0–180°) into a pulse length the driver understands
 int angleToPulse(int);
 
 void pointFinger();
@@ -45,5 +31,7 @@ void menu();
 void neutral();
 
 void peaceSign();
+
+void rockOn();
 
 #endif
